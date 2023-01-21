b5efd108a3b13216d0fd05648ffcc37a506e4295
hugo <hgk9872@gmail.com>
2023-01-22 04:22:30 +0900

    로그인, 로그아웃 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth import login as auth_login
 from .forms import SignupForm
+
+
+def logout(request):
+    messages.success(request, '로그아웃되었습니다')
+    return logout_then_login(request)
+
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
 
 
 def signup(request):
@@ -8,6 +18,7 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             signed_user = form.save()  # 새로 가입한 유저
+            auth_login(request, signed_user)
             signed_user.send_welcome_email()  # FIXME: Celery로 처리하는 것을 추천
             messages.success(request, "회원가입을 환영합니다")
             next_url = request.GET.get('next', '/')
