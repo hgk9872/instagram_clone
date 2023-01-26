094adea04804c9bab1d963c34a03287c5f0ef322
hugo <hgk9872@gmail.com>
2023-01-26 16:38:25 +0900

    유저 페이지 구현

--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
@@ -15,7 +16,7 @@ def post_new(request):
             post.save()
             post.tag_set.add(*post.extract_tag_list())
             messages.success(request, "포스팅을 저장했습니다")
-            return redirect("/")  # TODO: get_absolute_url 활용
+            return redirect(post)  # TODO: get_absolute_url 활용
     else:
         form = PostForm()
     return render(request, "instagram/post_form.html", context={
@@ -28,4 +29,14 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "instagram/post_detail.html", {
         'post': post,
+    })
+
+
+def user_page(request, username):
+    page_user = get_object_or_404(get_user_model(), username=username) # 해당 모델에 username 키워드 인자로 GET 요청
+    post_list = Post.objects.filter(author=page_user)
+    # post_list_count = post_list.count()
+    return render(request, "instagram/user_detail.html", {
+        'page_user': page_user,
+        'post_list': post_list,
     })
