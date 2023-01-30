76066f82029c4a0a78bbe9a74e7dfcae9b2a514a
hugo <hgk9872@gmail.com>
2023-01-30 23:08:47 +0900

    timeline에 포스팅 목록 구현

--- instagram/views.py
+++ instagram/views.py
@@ -2,15 +2,23 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from .forms import PostForm
 from .models import Tag, Post
 
 
 def index(request):
+    post_list = Post.objects.all()\
+        .filter(
+        Q(author=request.user) |
+        Q(author__in=request.user.following_set.all())
+    )
+
     suggested_user_list = get_user_model().objects.all()\
         .exclude(pk=request.user.pk)\
         .exclude(pk__in=request.user.following_set.all()) # 현재 following한 사람들은 보여주지 않음
     return render(request, "instagram/index.html", {
+        'post_list': post_list,
         'suggested_user_list': suggested_user_list[:5], # 추천친구 5명까지만
     })
 
